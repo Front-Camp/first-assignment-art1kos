130bf2299cc207af76964d63e0e37f1d8003dee8
art1kos <arturkos54@gmail.com>
2018-09-13 20:29:26 +0300

    test_Artur_Kosyiuk

--- src/getFlags/index.js
+++ src/getFlags/index.js
@@ -6,7 +6,13 @@
  * getFlags(arr); // [1, 2, 3, 4, 5, 6]
  */
 const getFlags = arr => {
-  	
+  	var arrsum = [];
+  for(let i = 0; i<arr.length; i++){
+      for(let j = 0; j<arr[i].flags.length; j++){
+          arrsum.push(arr[i].flags[j]);
+      }
+  }
+  return arrsum;
 };
 
 export default getFlags;
